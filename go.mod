196dff4e6a575551907334bf5b415c82be817ee9
amirhnajafiz <najafizadeh21@gmail.com>
2022-12-11 14:39:51 +0330

    feat: koanf

--- go.mod
+++ go.mod
@@ -6,16 +6,23 @@ require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/go-redis/redis/v9 v9.0.0-rc.2 // indirect
 	github.com/gofiber/fiber/v2 v2.40.1 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/knadh/koanf v1.4.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/superoo7/go-gecko v1.0.0 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.41.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,16 @@
 package config
 
 import (
+	"encoding/json"
+	"log"
+
 	"github.com/ceit-aut/cryptometer/internal/services/crypto"
 	"github.com/ceit-aut/cryptometer/internal/storage"
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+	"github.com/tidwall/pretty"
 )
 
 // Config stores application configs.
@@ -14,5 +22,36 @@ type Config struct {
 
 // Load configs.
 func Load() Config {
-	return Default()
+	var instance Config
+
+	k := koanf.New(".")
+
+	// load default configuration from file
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		log.Fatalf("error loading default: %s", err)
+	}
+
+	// load configuration from file
+	if err := k.Load(file.Provider("config.yml"), yaml.Parser()); err != nil {
+		log.Printf("error loading config.yml: %s", err)
+	}
+
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %s", err)
+	}
+
+	indent, err := json.MarshalIndent(instance, "", "\t")
+	if err != nil {
+		log.Fatalf("error marshaling config to json: %s", err)
+	}
+
+	indent = pretty.Color(indent, nil)
+	tmpl := `
+	================ Loaded Configuration ================
+	%s
+	======================================================
+	`
+	log.Printf(tmpl, string(indent))
+
+	return instance
 }
